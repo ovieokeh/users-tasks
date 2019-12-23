4972a00b8e8d6c4b549e446ec16a0bfcbb1e538a
Ovie Okeh <kevinokeh@gmail.com>
2019-12-23 18:47:10 +0100

    add basic error handling to users service

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -2,30 +2,48 @@ const UsersService = require('../services/users')
 
 class UsersController {
   static getAllUsers(_, res) {
-    UsersService.getAllUsers().then(users => {
-      res.status(200).json(users)
-    })
+    UsersService.getAllUsers()
+      .then(users => {
+        res.status(200).json(users)
+      })
+      .catch(err => {
+        res.status(500).json({
+          message: err.message
+        })
+      })
   }
 
   static updateUser(req, res) {
     const { userId } = req.params
     const { updatedName } = req.body
 
-    UsersService.updateUser(userId, updatedName).then(() => {
-      res.status(200).json({
-        message: 'user name updated successfully'
+    UsersService.updateUser(userId, updatedName)
+      .then(() => {
+        res.status(200).json({
+          message: 'user name updated successfully'
+        })
       })
-    })
+      .catch(err => {
+        res.status(500).json({
+          message: err.message
+        })
+      })
   }
 
   static deleteUser(req, res) {
     const { userId } = req.params
 
-    UsersService.deleteUser(userId).then(() => {
-      res.status(200).json({
-        message: 'user deleted successfully'
+    UsersService.deleteUser(userId)
+      .then(() => {
+        res.status(200).json({
+          message: 'user deleted successfully'
+        })
       })
-    })
+      .catch(err => {
+        res.status(500).json({
+          message: err.message
+        })
+      })
   }
 }
 
